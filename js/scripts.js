a40e38ef373e20c56757f69e9f0812813908c8d9
fortuna-es <56885624+fortuna-es@users.noreply.github.com>
2021-12-08 17:50:17 +0300

    added JS and HTML files

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(".footer-links-wrapper h3").click(function () {
+    if ($(window).width() <= 768) {
+      $(this).next("ul").slideToggle();
+      $(this).toggleClass("iconRotator");
+    }
+  });
